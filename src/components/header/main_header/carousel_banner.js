2810aee57f82b1d362f63ff331e84095c5941f69
DenisMiszeniow <duminocorp@gmail.com>
2022-06-03 22:48:33 +0200

    slider ready

--- src/components/header/main_header/carousel_banner.js
+++ src/components/header/main_header/carousel_banner.js
@@ -3,22 +3,24 @@ import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import slideOne from '../../../images/motoblok_slide.jpg'
+import slideTwo from '../../../images/traktor_slider_tiny.jpg'
+import slideThree from '../../../images/izdeliya-iz-provoloki_slide.jpg'
 
 class BannerCarousel extends Component {
     render() {
         return (
-            <Carousel onChange={onChange}>
+            <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false} showIndicators={false} showStatus={false} >
                 <div>
                     <img src={slideOne} />
-                    <p className="legend">Legend 1</p>
+                   
                 </div>
                 <div>
-                    <img src={slideOne} />
-                    <p className="legend">Legend 2</p>
+                    <img src={slideTwo} />
+                    
                 </div>
                 <div>
-                    <img src={slideOne} />
-                    <p className="legend">Legend 3</p>
+                    <img src={slideThree} />
+                    
                 </div>
             </Carousel>
         );
