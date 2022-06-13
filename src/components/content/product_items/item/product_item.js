eff735c94744b5f0e7873f3d990b1d1b15e9605c
DenisMiszeniow <duminocorp@gmail.com>
2022-06-13 12:31:21 +0200

    a new version of product carousel after debuging

--- src/components/content/product_items/item/product_item.js
+++ src/components/content/product_items/item/product_item.js
@@ -9,7 +9,6 @@ import { ItemGalleryCarousel } from './item_image_carousel'
 const ProductItem = (props) => {
 
     return (
-        <div>
             <div className={styles.productDiv}>
                 <div className={styles.titleDiv}>
                     <h3>{props.title}</h3>
@@ -17,19 +16,27 @@ const ProductItem = (props) => {
                 <div className={styles.container}>
                     <div>
                         <ItemGalleryCarousel image={props.foto}/>
-                    </div>
-                    <div>
+                    </div>    
+                    <div className={styles.discriptionContainer}>
                         <p>
                             {props.discription.discriotionText}
                         </p>
                         <strong>Технические характеристики:</strong>
+                        <ul className={styles.discriptionList}>
                         {
-                            props.discription.techData.map(data => <div><span>{`${data.DataName}: ${data.DataParameter}`}</span></div>)
+                            props.discription.techData.map(data => <li>
+                                                                        <span className={styles.discriptionName}>
+                                                                            {`${data.DataName}:`}
+                                                                        </span>
+                                                                        <span>{` ${data.DataParameter}`}
+                                                                        </span>
+                                                                    </li>)
                         }
+                        </ul>
                     </div>
                 </div>
             </div>
-        </div>
+        
     )
 }
 export default ProductItem
