cd9f0745b6fd077fe33d0617e23ccaec7321b864
DenisMiszeniow <duminocorp@gmail.com>
2022-06-14 20:46:57 +0200

    add about us and shipping and payment component

--- src/components/content/content_side.js
+++ src/components/content/content_side.js
@@ -3,6 +3,8 @@ import styles from './content_side.module.css'
 import { ProductsCategories } from './products_page/main_products_page_container'
 import { Route, Routes } from 'react-router-dom'
 import { MotoblockItems, TractorItems, WireItems } from './product_items/product_item_container'
+import AboutUs from './about_us/about_us'
+import ShippingAndPayment from './shipping_payment/shipping_payment'
 
 
 const ContentSide = (props) => {
@@ -13,6 +15,8 @@ const ContentSide = (props) => {
                 <Route path='/motoblock' element={<MotoblockItems/>}/>
                 <Route path='/tractor' element={<TractorItems/>}/>
                 <Route path='/wire' element={<WireItems/>}/>
+                <Route path='/onas' element={<AboutUs/>}/>
+                <Route path='/dostavkaoplata' element={<ShippingAndPayment/>}/>
             </Routes>
         </div>
     )
